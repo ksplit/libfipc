5eb74c8242bc4ea2435e02be34266e7d3ce193de
Scotty Bauer <sbauer@eng.utah.edu>
2015-07-01 20:06:39 -0600

    Added timing code to RPC, and results
    Signed-off-by: Scotty Bauer <sbauer@eng.utah.edu>

--- current/tests/rpc/callee.c
+++ current/tests/rpc/callee.c
@@ -2,6 +2,37 @@
 
 static struct ttd_ring_channel *channel;
 
+static unsigned long RDTSC_START(void)
+{
+
+	unsigned cycles_low, cycles_high;
+
+	asm volatile ("CPUID\n\t"
+		      "RDTSC\n\t"
+		      "mov %%edx, %0\n\t"
+		      "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+		      "%rax", "%rbx", "%rcx", "%rdx");
+	return ((unsigned long) cycles_high << 32) | cycles_low;
+
+}
+
+
+static unsigned long RDTSCP(void)
+{
+	unsigned long tsc;
+	__asm__ __volatile__(
+        "rdtscp;"
+        "shl $32, %%rdx;"
+        "or %%rdx, %%rax"
+        : "=a"(tsc)
+        :
+        : "%rcx", "%rdx");
+
+	return tsc;
+}
+
+
+
 static unsigned long add_constant(unsigned long trans)
 {
 	struct ipc_message *msg;
@@ -12,9 +43,6 @@ static unsigned long add_constant(unsigned long trans)
 	msg->reg1 = trans;
 	send(channel,msg);
 	msg = recv(channel);
-	if (msg->fn_type != ADD_CONSTANT)
-		pr_err("Response was not ADD_CONSTANT");
-
 	result = msg->reg1;
 	transaction_complete(msg);
 	return result;
@@ -31,9 +59,6 @@ static unsigned long add_nums(unsigned long trans, unsigned long res1)
 	msg->reg2 = res1;
 	send(channel,msg);
 	msg = recv(channel);
-	if (msg->fn_type != ADD_NUMS)
-		pr_err("Response was not ADD_NUMS");
-
 	result = msg->reg1;
 	transaction_complete(msg);
 	return result;
@@ -53,9 +78,6 @@ static unsigned long add_3_nums(unsigned long trans, unsigned long res1,
 	msg->reg3 = res2;
 	send(channel,msg);
 	msg = recv(channel);
-	if (msg->fn_type != ADD_3_NUMS)
-		pr_err("Response was not ADD_3_NUMS");
-
 	result = msg->reg1;
 	transaction_complete(msg);
 	return result;
@@ -75,9 +97,6 @@ static unsigned long add_4_nums(unsigned long trans, unsigned long res1,
 	msg->reg4 = res3;
 	send(channel,msg);
 	msg = recv(channel);
-	if (msg->fn_type != ADD_4_NUMS)
-		pr_err("Response was not ADD_4_NUMS");
-
 	result = msg->reg1;
 	transaction_complete(msg);
 	return result;
@@ -99,9 +118,6 @@ static unsigned long add_5_nums(unsigned long trans, unsigned long res1,
 	msg->reg5 = res4;
 	send(channel,msg);
 	msg = recv(channel);
-	if (msg->fn_type != ADD_5_NUMS)
-		pr_err("Response was not ADD_5_NUMS");
-
 	result = msg->reg1;
 	transaction_complete(msg);
 	return result;
@@ -137,14 +153,18 @@ void callee(struct ttd_ring_channel *chan)
 {
 	unsigned long num_transactions = 0;
 	unsigned long res1, res2, res3, res4, res5, res6;
+	unsigned long start,end;
 	channel = chan;
 	while (num_transactions < TRANSACTIONS) {
+		start = RDTSC_START();
 		res1 = add_constant(num_transactions);
 		res2 = add_nums(num_transactions,res1);
 		res3 = add_3_nums(num_transactions,res1,res2);
 		res4 = add_4_nums(num_transactions,res1,res2,res3);
 		res5 = add_5_nums(num_transactions,res1,res2,res3,res4);
 		res6 = add_6_nums(num_transactions,res1,res2,res3,res4,res5);
+		end = RDTSCP();
+		pr_err("%lu\n", end-start);
 		num_transactions += 6;
 		//pr_err("res6 is %lu\n on iteration, %lu",res6, num_transactions);
 	}
--- current/tests/rpc/rpc.h
+++ current/tests/rpc/rpc.h
@@ -6,7 +6,7 @@ typedef enum {ADD_CONSTANT, ADD_NUMS, ADD_3_NUMS,
 	      ADD_4_NUMS, ADD_5_NUMS, ADD_6_NUMS} fn_types;
 
 /* must be divisible by 6... because I call 6 functions in the Callee.c */
-#define TRANSACTIONS 36000000
+#define TRANSACTIONS 12000
 
 
 void callee(struct ttd_ring_channel *chan);
