a2ad2a05bb40080635e98a01321a2c1cd6dd0b7e
Riyt <younisa@uci.edu>
2016-12-16 20:37:21 -0800

    Starting to cross the bridge to userland

--- src/platform/userland/include/libfipc_platform_internal.h
+++ src/platform/userland/include/libfipc_platform_internal.h
@@ -1,17 +1,17 @@
 /*
  * libfipc_platform_internal.h
  *
- * libfipc internal kernel-specific defs.
+ * libfipc internal Userland-specific defs.
  *
  * Copyright: University of Utah
  */
 #ifndef LIBFIPC_PLATFORM_INTERNAL_H
 #define LIBFIPC_PLATFORM_INTERNAL_H
 
-#include <linux/printk.h>
+#include <stdio.h>
 
 #define __fipc_debug(fmt, ...) \
-    printk(KERN_ERR "fipc: %s:%d: "fmt,__FUNCTION__,__LINE__,##__VA_ARGS__)
+    printf("fipc: %s:%d: "fmt,__FUNCTION__,__LINE__,##__VA_ARGS__)
 
 #define __FIPC_BUILD_BUG_ON_NOT_POWER_OF_2(x) (__FIPC_BUILD_BUG_ON((x) == 0 || (((x) & ((x) - 1)) != 0)))
 #define __FIPC_BUILD_BUG_ON(x) ((void)sizeof(char[1 - 2*!!(x)]))
