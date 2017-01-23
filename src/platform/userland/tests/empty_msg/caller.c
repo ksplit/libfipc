8a39280e7606ade5ac83a74fb8eef066c9889f4d
Anton Burtsev <aburtsev@flux.utah.edu>
2017-01-22 19:53:25 -0800

    Remove printf from inside the main loop
      -- Accumulate measurements, then print them out
      -- Doesn't change the result though...

--- src/platform/userland/tests/empty_msg/caller.c
+++ src/platform/userland/tests/empty_msg/caller.c
@@ -46,9 +46,9 @@ int do_one_msg ( Header* chan )
 
 void* caller(void *_caller_channel_header)
 {
-    Header* chan = _caller_channel_header;
+	Header* chan = _caller_channel_header;
 	unsigned long transaction_id;
-	unsigned long start, end;
+	unsigned long start[TRANSACTIONS], end[TRANSACTIONS];
 	uint64_t avgDelay = test_Average_Stopwatch_Delay();
 	printf("AvgDelay: %lu\n", avgDelay);
 	int ret = 0;
@@ -63,17 +63,23 @@ void* caller(void *_caller_channel_header)
 	     transaction_id < TRANSACTIONS;
 	     transaction_id++)
 	{
-		start = test_fipc_start_stopwatch();
+		start[transaction_id] = test_fipc_start_stopwatch();
 		ret = do_one_msg(chan);
-		end = test_fipc_stop_stopwatch();
+		end[transaction_id] = test_fipc_stop_stopwatch();
 
 		if (ret)
 		{
 			fprintf(stderr, "error in send/recv, ret = %d, exiting...\n", ret);
 			pthread_exit(&ret);
 		}
+	}
 
-		printf("\t%lu\n", end - start - avgDelay);
+	
+	for (transaction_id = 0;
+             transaction_id < TRANSACTIONS;
+             transaction_id++)
+        {
+		printf("\t%lu\n", end[transaction_id] - start[transaction_id] - avgDelay);
 	}
 
 	printf("%s\n", "Complete");
