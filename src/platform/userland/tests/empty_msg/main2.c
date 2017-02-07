e9ae44fc04c92dcaaf97b2d39bef3bbdf68865cd
Riyt <younisa@uci.edu>
2017-02-06 16:06:20 -0800

    Brute way of sending libfipc messages

---
+++ src/platform/userland/tests/empty_msg/main2.c
@@ -0,0 +1,188 @@
+/**
+ * @File     : main.c
+ * @Author   : Charles Jacobsen
+ * @Author   : Abdullah Younis
+ * @Copyright: University of Utah
+ *
+ * This test sends an empty message in a round trip using the fipc library.
+ *
+ * NOTE: This program assumes an x86 architecture.
+ */
+
+#include "../libfipc_test.h"
+
+#define TRANSACTIONS	10000
+#define REQUESTER_CPU	1
+#define RESPONDER_CPU	3
+#define CHANNEL_ORDER	ilog2(sizeof(struct fipc_message))
+
+#define AVAILABLE 0xdeadU
+#define SENT      0xfeedU
+
+// Thread Locks
+pthread_mutex_t requester_mutex;
+pthread_mutex_t responder_mutex;
+
+void request ( header_t* chan )
+{
+	while ( chan->tx.buffer->msg_status != AVAILABLE )
+		fipc_test_pause();
+
+	// Send request (GetM)
+	chan->tx.buffer->msg_status = SENT;
+
+	// Wait until message is received (GetS)
+	while ( chan->rx.buffer->msg_status != SENT )
+		fipc_test_pause();
+
+	// Receive response (GetM)
+	chan->rx.buffer->msg_status = AVAILABLE;
+
+	/* message_t* request;
+	message_t* response;
+
+	// Wait until message is available (GetS)
+	fipc_test_blocking_send_start( chan, &request );
+
+	// Send request (GetM)
+	fipc_send_msg_end ( chan, request );
+
+	// Wait until response is received (GetS)
+	fipc_test_blocking_recv_start( chan, &response );
+
+	// Receive response (GetM)
+	fipc_recv_msg_end( chan, response );*/
+}
+
+void respond ( header_t* chan )
+{
+	// Wait until message is received (GetS)
+	while ( chan->rx.buffer->msg_status != SENT )
+		fipc_test_pause();
+
+	// Receive request (GetM)
+	chan->rx.buffer->msg_status = AVAILABLE;
+
+
+	// Wait until message is available (GetS)
+	while ( chan->tx.buffer->msg_status != AVAILABLE )
+		fipc_test_pause();
+
+	// Send response (GetM)
+	chan->tx.buffer->msg_status = SENT;
+
+	/*message_t* request;
+	message_t* response;
+
+	// Wait until request is received (GetS)
+	fipc_test_blocking_recv_start( chan, &request );
+
+	// Receive request (GetM)
+	fipc_recv_msg_end( chan, request );
+
+	// Wait until message is available (GetS)
+	fipc_test_blocking_send_start( chan, &response );
+
+	// Send response (GetM)
+	fipc_send_msg_end( chan, response );*/
+}
+
+void* requester ( void* data )
+{
+	uint64_t  start;
+	uint64_t  end;
+	uint64_t  transaction_id;
+	float     sum  = 0;
+	header_t* chan = (header_t*) data;
+	
+	// Wait to begin test
+	pthread_mutex_lock( &requester_mutex );
+
+	// Begin test
+	for ( transaction_id = 0; transaction_id < TRANSACTIONS; transaction_id++ )
+	{
+		start = fipc_test_time_get_timestamp();
+		request( chan );
+		end   = fipc_test_time_get_timestamp(); // A memory fence here costs ~200 cycles
+
+		printf("\t%lu\n", end - start);
+		sum += end - start;
+	}
+
+	// End test
+	printf( "Average Round Trip Cycles:\t%f\n", sum / TRANSACTIONS );
+	pthread_mutex_unlock( &requester_mutex );
+	pthread_exit( 0 );
+	return NULL;
+}
+
+void* responder ( void* data )
+{
+	uint64_t  transaction_id;
+	header_t* chan = (header_t*) data;
+	
+	// Wait to begin test
+	pthread_mutex_lock( &responder_mutex );
+
+
+	for ( transaction_id = 0; transaction_id < TRANSACTIONS; transaction_id++ )
+	{
+		respond( chan );
+	}
+
+	pthread_exit( 0 );
+	return NULL;
+}
+
+
+int main ( void )
+{
+	// Begin critical section
+	pthread_mutex_init( &requester_mutex, NULL );
+	pthread_mutex_init( &responder_mutex, NULL );
+	
+	pthread_mutex_lock( &requester_mutex );
+	pthread_mutex_lock( &responder_mutex );
+	
+	header_t* requester_header;
+	header_t* responder_header;
+
+	fipc_init();
+	fipc_test_create_channel( CHANNEL_ORDER,
+								&requester_header,
+								&responder_header );
+	// Create Threads
+	pthread_t* requester_thread = fipc_test_thread_spawn_on_CPU ( requester,
+																requester_header,
+																REQUESTER_CPU );
+	if ( requester_thread == NULL )
+	{
+		fprintf( stderr, "%s\n", "Error while creating thread" );
+		return -1;
+	}
+	
+	pthread_t* responder_thread = fipc_test_thread_spawn_on_CPU ( responder,
+																responder_header,
+																RESPONDER_CPU );
+	if ( responder_thread == NULL )
+	{
+		fprintf( stderr, "%s\n", "Error while creating thread" );
+		return -1;
+	}
+	
+	// End critical section = start threads
+	pthread_mutex_unlock( &responder_mutex );
+	pthread_mutex_unlock( &requester_mutex );
+	
+	// Wait for thread completion
+	fipc_test_thread_wait_for_thread( requester_thread );
+	fipc_test_thread_wait_for_thread( responder_thread );
+	
+	// Clean up
+	fipc_test_thread_free_thread( requester_thread );
+	fipc_test_thread_free_thread( responder_thread );
+	fipc_test_free_channel( CHANNEL_ORDER, requester_header, responder_header );
+	fipc_fini();
+	pthread_exit( NULL );
+	return 0;
+}
