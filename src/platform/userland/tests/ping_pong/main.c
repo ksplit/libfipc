377eb860065018e3d3e8ab6ceeabb254fbaa6cf2
Anton Burtsev <aburtsev@flux.utah.edu>
2017-02-09 19:33:00 -0800

    More debugging
      -- Introduce one cache-linte test
      -- Simple cache_line variable

--- src/platform/userland/tests/ping_pong/main.c
+++ src/platform/userland/tests/ping_pong/main.c
@@ -14,7 +14,7 @@
  #include "../libfipc_test.h"
 
 #define REQUESTER_CPU 0
-#define RESPONDER_CPU 2
+#define RESPONDER_CPU 3
 #define TRANSACTIONS  1000000
 #define AVAILABLE 0
 #define SENT      1
@@ -22,8 +22,11 @@
 // Global Variables = Test Shared Memory
 pthread_mutex_t requester_mutex;
 pthread_mutex_t responder_mutex;
-volatile cache_line_t reqt_buffer = { .msg_status = 0} ;
-volatile cache_line_t resp_buffer = { .msg_status = 0} ;
+//volatile cache_line_t reqt_buffer = { .msg_status = 0} ;
+//volatile cache_line_t resp_buffer = { .msg_status = 0} ;
+volatile unsigned long __attribute__((aligned(64))) req_line;
+volatile unsigned long __attribute__((aligned(64))) resp_line;
+
 
 #define likely(x)       __builtin_expect(!!(x), 1)
 #define unlikely(x)     __builtin_expect(!!(x), 0)
@@ -31,17 +34,20 @@ volatile cache_line_t resp_buffer = { .msg_status = 0} ;
 unsigned long __attribute__((aligned(64))) resp_sequence = 1; 
 unsigned long __attribute__((aligned(64))) req_sequence = 1;
 
-static inline void request ( void )
+static inline void request_2_lines ( void )
 {
 	// Wait until message is available (GetS)
 	//while ( reqt_buffer.msg_status != AVAILABLE )
 	//	fipc_test_pause();
 
 	// Send request (GetM)
-	reqt_buffer.msg_status = req_sequence;
+	//reqt_buffer.msg_status = req_sequence;
+    req_line = req_sequence;
 
 	// Wait until message is received (GetS)
-	while ( likely(resp_buffer.msg_status != req_sequence) )
+	//while ( likely(resp_buffer.msg_status != req_sequence) )
+	while ( likely(resp_line != req_sequence) )
+
 		fipc_test_pause();
 
 	// Receive response (GetM)
@@ -50,10 +56,12 @@ static inline void request ( void )
 	//printf("req_seq:%lu\n", req_sequence); 
 }
 
-static inline void respond ( void )
+static inline void respond_2_lines ( void )
 {
 	// Wait until message is received (GetS)
-	while ( likely(reqt_buffer.msg_status != resp_sequence ))
+	//while ( likely(reqt_buffer.msg_status != resp_sequence ))
+    while ( likely(req_line != resp_sequence ))
+
 		fipc_test_pause();
 
 	// Receive request (GetM)
@@ -65,22 +73,70 @@ static inline void respond ( void )
 	//	fipc_test_pause();
 
 	// Send response (GetM)
-	resp_buffer.msg_status = resp_sequence;
+	//resp_buffer.msg_status = resp_sequence;
+	resp_line = resp_sequence;
+
 	//reqt_buffer.msg_status = AVAILABLE;
 	resp_sequence ++;
 	//printf("resp_seq:%lu\n", resp_sequence);
 }
 
+static inline void request ( void )
+{
+	// Wait until message is available (GetS)
+	//while ( reqt_buffer.msg_status != AVAILABLE )
+	//	fipc_test_pause();
+
+	// Send request (GetM)
+	//reqt_buffer.msg_status = req_sequence;
+	req_line = req_sequence;
+
+	// Wait until message is received (GetS)
+	//while ( unlikely(reqt_buffer.msg_status != (req_sequence + 1)))
+	while ( unlikely(req_line != (req_sequence + 1)))
+
+		fipc_test_pause();
+
+	// Receive response (GetM)
+	//resp_buffer.msg_status = AVAILABLE;
+	req_sequence += 2;
+	//printf("req_seq:%lu\n", req_sequence); 
+}
+
+static inline void respond ( void )
+{
+	// Wait until message is received (GetS)
+	//while ( unlikely(reqt_buffer.msg_status != resp_sequence ))
+	while ( unlikely(req_line != resp_sequence ))
+
+		fipc_test_pause();
+
+	// Receive request (GetM)
+	//reqt_buffer.msg_status = AVAILABLE;
+
+
+	// Wait until message is available (GetS)
+	//while ( resp_buffer.msg_status != AVAILABLE )
+	//	fipc_test_pause();
+
+	// Send response (GetM)
+	//reqt_buffer.msg_status = resp_sequence + 1;
+	req_line = resp_sequence + 1;
+	//reqt_buffer.msg_status = AVAILABLE;
+	resp_sequence += 2;
+	//printf("resp_seq:%lu\n", resp_sequence);
+}
 void* requester ( void* data )
 {
-	uint64_t start;
-	uint64_t end;
+	uint64_t __attribute__((aligned(64))) start;
+	uint64_t __attribute__((aligned(64))) end;
 	uint32_t __attribute__((aligned(64))) transaction_id;
-	unsigned long long sum = 0;
+	unsigned long long __attribute__((aligned(64))) sum = 0;
 	
 	// Wait to begin test
 	pthread_mutex_lock( &requester_mutex );
 	start = RDTSC_START();
+//	start = fipc_test_time_get_timestamp();
 
 	for ( transaction_id = 0; transaction_id < TRANSACTIONS; transaction_id++ )
 	{
@@ -88,8 +144,9 @@ void* requester ( void* data )
 		//start = RDTSC_START();
               
 		//start = fipc_test_time_get_timestamp();
-		request();
-		//end   = fipc_test_time_get_timestamp(); // A memory fence here costs ~200 cycles
+        request();
+		//request_2_lines();
+		//vend   = fipc_test_time_get_timestamp(); // A memory fence here costs ~200 cycles
 
                 //resp_buffer.msg_status = AVAILABLE;        
 		//end = RDTSCP();
@@ -97,6 +154,8 @@ void* requester ( void* data )
        
 		//printf("\t%lu\n", end - start);
 	}
+//	end   = fipc_test_time_get_timestamp(); // A memory fence here costs ~200 cycles
+
 	end = RDTSCP();
 	sum += end - start;
 
@@ -116,9 +175,8 @@ void* responder ( void* data )
 	uint32_t __attribute__((aligned(64))) transaction_id;
 	for ( transaction_id = 0; transaction_id < TRANSACTIONS; transaction_id++ )
 	{
-		respond(); 
-                //reqt_buffer.msg_status = AVAILABLE;
-
+		//respond_2_lines(); 
+		respond();
 	}
 
 	pthread_mutex_unlock( &responder_mutex );
