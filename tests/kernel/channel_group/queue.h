038d03ac26dc4454da970ae8d5b82fa921a40561
Abdullah Younis <younisa@uci.edu>
2018-02-01 21:15:38 -0800

    channel_group test

---
+++ tests/kernel/channel_group/queue.h
@@ -0,0 +1,37 @@
+/**
+ * @File     : queue.h
+ * @Author   : Abdullah Younis
+ */
+
+#ifndef LIBFIPC_TEST_QUEUE
+#define LIBFIPC_TEST_QUEUE
+
+#include "../libfipc_test.h"
+
+#define CHANNEL_ORDER ilog2(sizeof(message_t)) + 13
+
+// Error Values
+#define SUCCESS              0
+#define NO_MEMORY            1
+#define EMPTY_COLLECTION     2
+
+// Types
+typedef uint64_t data_t;
+
+typedef message_t node_t;
+
+typedef struct queue_t
+{
+	header_t* head;
+	header_t* tail;
+
+} queue_t;
+
+int init_queue   ( queue_t* q );
+int free_queue   ( queue_t* q );
+int enqueue_forw ( queue_t* q, node_t* n );
+int dequeue_forw ( queue_t* q, data_t* d );
+int enqueue_back ( queue_t* q, node_t* n );
+int dequeue_back ( queue_t* q, data_t* d );
+
+#endif
