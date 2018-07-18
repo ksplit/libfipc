63c6acb3af60b2d3a8e557a6b9e8e42cb278b3df
Anton Burtsev <aburtsev@flux.utah.edu>
2018-07-18 10:46:42 -0700

    Revert "Update gitignore" (restore deleted files)
    This reverts commit 821752e2ca9f9d432b28670144c488075f56a7fc.

---
+++ tests/userland/ping_pong_bare_1/main.c
@@ -0,0 +1,125 @@
+/**
+ * @File     : main.c
+ * @Author   : Abdullah Younis
+ */
+
+#include "test.h"
+
+static inline
+void request ( void )
+{
+	// Write Request
+	line.regs[0] = req_sequence++;
+
+	// Read Response
+	while ( unlikely( line.regs[0] != req_sequence ) )
+		fipc_test_pause();
+
+	req_sequence++;
+}
+
+static inline
+void respond ( void )
+{
+	// Read Request
+	while ( unlikely( line.regs[0] != resp_sequence ) )
+		fipc_test_pause();
+
+	// Write Response
+	line.regs[0] = ++resp_sequence;
+	resp_sequence++;
+}
+
+void* requester ( void* data )
+{
+	register uint64_t CACHE_ALIGNED transaction_id;
+	register uint64_t CACHE_ALIGNED start;
+	register uint64_t CACHE_ALIGNED end;
+	register uint64_t CACHE_ALIGNED correction = fipc_test_time_get_correction();
+	register int64_t* CACHE_ALIGNED times = malloc( TRANSACTIONS * sizeof( int64_t ) );
+	
+	// Wait to begin test
+	pthread_mutex_lock( &requester_mutex );
+
+	for ( transaction_id = 0; transaction_id < TRANSACTIONS; transaction_id++ )
+	{
+		start = RDTSC_START();
+
+		request();
+
+		end = RDTSCP();
+		times[transaction_id] = (end - start) - correction;
+	}
+
+	// End test
+	fipc_test_stat_get_and_print_stats( times, TRANSACTIONS );
+	free( times );
+	pthread_mutex_unlock( &requester_mutex );
+	pthread_exit( 0 );
+	return NULL;
+}
+
+void* responder ( void* data )
+{
+	register uint64_t CACHE_ALIGNED transaction_id;
+
+	// Wait to begin test
+	pthread_mutex_lock( &responder_mutex );
+
+	for ( transaction_id = 0; transaction_id < TRANSACTIONS; transaction_id++ )
+	{
+		respond();
+	}
+
+	// End test
+	pthread_mutex_unlock( &responder_mutex );
+	pthread_exit( 0 );
+	return NULL;
+}
+
+
+int main ( void )
+{
+	// Init Variables
+	line.regs[0] = 0;
+	
+	// Begin critical section
+	pthread_mutex_init( &requester_mutex, NULL );
+	pthread_mutex_init( &responder_mutex, NULL );
+	
+	pthread_mutex_lock( &requester_mutex );
+	pthread_mutex_lock( &responder_mutex );
+	
+	// Create Threads
+	pthread_t* requester_thread = fipc_test_thread_spawn_on_CPU ( requester,
+																NULL,
+																REQUESTER_CPU );
+	if ( requester_thread == NULL )
+	{
+		fprintf( stderr, "%s\n", "Error while creating thread" );
+		return -1;
+	}
+	
+	pthread_t* responder_thread = fipc_test_thread_spawn_on_CPU ( responder,
+																NULL,
+																RESPONDER_CPU );
+	if ( responder_thread == NULL )
+	{
+		fprintf( stderr, "%s\n", "Error while creating thread" );
+		return -1;
+	}
+	
+	// End critical section = start threads
+	pthread_mutex_unlock( &responder_mutex );
+	pthread_mutex_unlock( &requester_mutex );
+	
+	// Wait for thread completion
+	fipc_test_thread_wait_for_thread( requester_thread );
+	fipc_test_thread_wait_for_thread( responder_thread );
+	
+	// Clean up
+	fipc_test_thread_free_thread( requester_thread );
+	fipc_test_thread_free_thread( responder_thread );
+	pthread_exit( NULL );
+	return 0;
+}
---
+++ tests/userland/ping_pong_bare_2/test.h
@@ -0,0 +1,27 @@
+/**
+ * @File     : test.h
+ * @Author   : Abdullah Younis
+ *
+ * NOTE: This test assumes an x86 architecture.
+ */
+
+#ifndef LIBFIPC_TEST_PING_PONG_BARE_2
+#define LIBFIPC_TEST_PING_PONG_BARE_2
+
+#include "../libfipc_test.h"
+
+#define TRANSACTIONS	10000000
+#define REQUESTER_CPU	0
+#define RESPONDER_CPU	2
+
+// Thread Locks
+pthread_mutex_t requester_mutex;
+pthread_mutex_t responder_mutex;
+
+volatile cache_line_t CACHE_ALIGNED req_line;
+volatile cache_line_t CACHE_ALIGNED resp_line;
+
+volatile uint64_t CACHE_ALIGNED resp_sequence = 1; 
+volatile uint64_t CACHE_ALIGNED req_sequence  = 1;
+
+#endif
