0ca1370e706943657cf3d1f57e168d25197b8a80
Jeonghoon Lee <jeonghl9@uci.edu>
2018-07-25 17:35:46 -0700

    Locked queue
    Signed-off-by: Jeonghoon Lee <jeonghl9@uci.edu>

--- tests/userland/queue_mcslock/queue.c
+++ tests/userland/queue_mcslock/queue.c
@@ -100,6 +100,8 @@ int init_queue ( queue_t* q )
 		pr_err( "%s\n", "Error while creating channel" );
 		return -1;
 	}
+	mcs_init( &(q->H_lock) );
+	mcs_init( &(q->T_lock) );
 
 	return SUCCESS;
 
@@ -116,12 +118,17 @@ int free_queue ( queue_t* q )
 int enqueue ( queue_t* q, node_t* node )
 {
 	message_t* msg;
+	mcs_lock( &(q->T_lock) );
 
 	if (fipc_send_msg_start( q->head, &msg ) != 0)
+	{
+		mcs_unlock( &(q->T_lock) );
 		return NO_MEMORY;
+	}
 
 	msg->regs[0] = (uint64_t)node;
 	fipc_send_msg_end ( q->head, msg );
+	mcs_unlock( &(q->T_lock) );
 
 	return SUCCESS;
 }
@@ -132,12 +139,17 @@ int enqueue ( queue_t* q, node_t* node )
 int dequeue ( queue_t* q, node_t** node )
 {
 	message_t* msg;
+	mcs_lock( &(q->H_lock) );
 
 	if (fipc_recv_msg_start( q->tail, &msg) != 0)
+	{
+		mcs_unlock( &(q->H_lock) );
 		return EMPTY_COLLECTION;
+	}
 
 	*node = (node_t*)msg->regs[0];
 	fipc_recv_msg_end( q->tail, msg );
+	mcs_unlock( &(q->H_lock) );
 
 	return SUCCESS;
 }
