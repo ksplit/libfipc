953c067284e585831e7e1c5d3ade8724512a9411
Jiwon Jeon <ji1@node-0.isurf2.redshift.emulab.net>
2018-10-21 01:53:30 -0600

    Change the location of on->off code section

--- tests/userland/hyper_exp_script/ht_on_off.sh
+++ tests/userland/hyper_exp_script/ht_on_off.sh
@@ -7,21 +7,23 @@ usage() { echo "Usage: $0 [-o <0|1>] [-h]" 1>&2; exit 1; }
 ht_set() {
   if [ $1 == 0 ]; then
     echo 'thread off'
-    # off -> off
-    if cat /sys/devices/system/cpu/offline | grep '-'; then
-      echo 'HT was already off'
-      exit 1
-    fi
     # on -> off
     cd /sys/devices/system/cpu
     cat ./cpu*/topology/thread_siblings_list | sort | uniq |
     while read -r line; do
       IFS=',' read -ra CORE <<< "$line"
       cd cpu${CORE[1]}
-      echo 1 | sudo tee online
+      sudo chmod 777 online
+      echo 0 > online
+      sudo chmod 444 online
       cd ..
     done 
     exit 1
+    # off -> off
+    if cat /sys/devices/system/cpu/offline | grep '-'; then
+      echo 'HT was already off'
+      exit 1
+    fi
   fi
   
   if [ $1 == 1 ]; then
