8b47c0adc70004eeb06b8d5e52fb37b022d966c6
Jiwon Jeon <ji1@node-0.isurf2.redshift.emulab.net>
2018-10-21 12:37:44 -0600

    Fix bugs

--- tests/userland/hyper_exp_script/ht_on_off.sh
+++ tests/userland/hyper_exp_script/ht_on_off.sh
@@ -2,63 +2,76 @@
 # @Description: This script turns on/off the sibling thread
 # 
 
-usage() { echo "Usage: $0 [-o <0|1>] [-h]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-o <0|1>] [-h] [-s]" 1>&2; exit 1; }
+
+ht_show() { echo "$(cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | sort | uniq)"; exit 1;}
 
 ht_set() {
-  if [ $1 == 0 ]; then
-    echo 'thread off'
-    # on -> off
-    cd /sys/devices/system/cpu
-    cat ./cpu*/topology/thread_siblings_list | sort | uniq |
-    while read -r line; do
-      IFS=',' read -ra CORE <<< "$line"
-      cd cpu${CORE[1]}
-      sudo chmod 777 online
-      echo 0 > online
-      sudo chmod 444 online
-      cd ..
-    done 
-    exit 1
-    # off -> off
-    if cat /sys/devices/system/cpu/offline | grep '-'; then
-      echo 'HT was already off'
-      exit 1
-    fi
-  fi
-  
-  if [ $1 == 1 ]; then
-    echo 'thread on'
-    # off -> on
-    if cat /sys/devices/system/cpu/offline | grep '-'; then
-      cd /sys/devices/system/cpu
-      IFS='-' read -ra CORE <<< "$(cat /sys/devices/system/cpu/offline)"
-      for i in $(seq ${CORE[0]} ${CORE[1]}); do
-        cd cpu$i
-        sudo chmod 777 online
-        echo 1 > online
-        sudo chmod 644 online
-        cd ..
-      done
-      exit 1
-    fi
-    # on -> on
-    echo 'HT was already on'
-    exit 1
-  fi
+  sudo chmod 777 online
+  echo $1 > online
+  sudo chmod 644 online
 }
 
-while getopts ":o:" opt; do
-  case ${opt} in
+ht_on() {
+  echo '[sibling thread on]'
+  # off -> on
+  if cat /sys/devices/system/cpu/offline | grep '-'; then
+   cd /sys/devices/system/cpu
+    IFS='-' read -ra PRESENT <<< "$(cat ./present)"
+    IFS=',' read -ra OFFLINE <<< "$(cat ./offline)"
+
+    # POSSIBLE CPUs might be different from PRESENT CPUs
+    for i in ${OFFLINE[@]}; do 
+      IFS='-' read -ra LIST <<< "$i"
+      for i in $(seq ${LIST[0]} ${LIST[1]}); do
+        if (( $i < ${PRESENT[1]} )); then 
+          cd cpu$i
+          ht_set 1 
+          cd ..
+        fi
+      done
+    done
+  fi
+  exit 1
+}
+
+ht_off() {
+  echo '[sibling thread off]'
+  # on -> off
+  cd /sys/devices/system/cpu
+  cat ./cpu*/topology/thread_siblings_list | sort | uniq |
+  while read -r line; do
+    IFS=',' read -ra CORE <<< "$line"
+    if [ ! -z ${CORE[1]} ];then
+      cd cpu${CORE[1]}
+      ht_set 0
+      cd ..
+    fi 
+  done 
+  exit 1
+}
+
+while getopts ":hso:" opt; do
+  case $opt in
     o)
       # -o 1: turn on sibling thread
+     if [ $OPTARG == 1 ]; then
+        ht_on
+      fi
+       
       # -o 0: turn off sibling thread
-      ht_set $OPTARG
+      if [ $OPTARG == 0 ]; then
+        ht_off
+      fi
       
       # wrong option
       echo 'wrong option'
       usage
       ;;
-    ?)
+    s)
+      ht_show
+      ;;
+    *)
       usage
       ;;
    esac
