d95a9c93357e4c2189df0cc79e559a3e7e01885f
Jiwon Jeon <ji1@node-0.debug.redshift.emulab.net>
2018-10-20 21:23:00 -0600

    HT turn on/off script -- still has bug

---
+++ tests/userland/hyper_exp_script/ht_on_off.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# @Description: This script turns on/off the sibling thread
+# 
+
+usage() { echo "Usage: $0 [-o <0|1>] [-h]" 1>&2; exit 1; }
+
+ht_set() {
+  if [ $1 == 0 ]; then
+    echo 'thread off'
+  fi
+  
+  if [ $1 == 1 ]; then
+    echo 'thread on'
+    if cat /sys/devices/system/cpu/offline | grep '-'; then
+      cd /sys/devices/system/cpu
+      IFS='-' read -ra CORE <<< "$(cat /sys/devices/system/cpu/offline)"
+      for i in $(seq ${CORE[0]} ${CORE[1]}); do
+        cd cpu$i
+        sudo chmod 777 online
+        echo 1 > online
+        sudo chmod 644 online
+        cd ..
+      done
+      exit 1
+    fi
+    echo 'HT was already on'
+  fi
+
+  cd /sys/devices/system/cpu
+  cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | sort | uniq |
+  while read -r line; do
+    IFS=',' read -ra CORE <<< "$line"
+    cd cpu${CORE[1]}
+    sudo chmod 777 online
+    sudo echo $1 > online
+    sudo chmod 644 online
+    cd ..
+  done 
+  exit 1
+
+}
+
+while getopts ":o:" opt; do
+  case ${opt} in
+    o)
+      # -o 1: turn on sibling thread
+      # -o 0: turn off sibling thread
+      ht_set $OPTARG
+      
+      # wrong option
+      echo 'wrong option'
+      usage
+      ;;
+    ?)
+      usage
+      ;;
+   esac
+done
