f732a934c58afb091b3b2a037b47de34694c896a
ji1 <wldnjs602@ajou.ac.kr>
2018-11-13 15:23:39 -0800

    Modify experiment running config json file

--- tests/userland/exp_script/exp.py
+++ tests/userland/exp_script/exp.py
@@ -2,32 +2,33 @@ import threading, requests
 import sys, os, time, shlex, signal, atexit
 import json, getopt
 import smtplib
+import io
 from email.mime.text import MIMEText
 from subprocess import *
 from time import localtime, strftime
 
 # Default profiling options
-RUN_BASE = "/users/ji1/fast-ipc-module/tests/userland"
+RUN_BASE = "/users/ji1/revert/tests/userland"
 TEST_LIST = [ 'queue_mpsc_mcslock' ]
-systemInfoFName = 'system.info'
 expInfoFName = 'exp.info'
 iterCnt = 1
 opt = [ 1 ]
-# usage
+
+# Show usage
 def usage():
 	print 'Usage: %s -c config.json' % sys.argv[0]
 
-# email notification
-
-def send_email(user, pwd):
+# Send google email notification
+def send_email(user, pwd, resultDir):
 
 	# content
 	try:
 		server = smtplib.SMTP("smtp.gmail.com", 587)
 		server.ehlo()
 		server.starttls()
-		server.login(user,pwd)
-		server.sendmail(user, user, 'Experiments finished')
+		server.login(user,pw)
+		msg= 'Experiments finished.\ndirectory: %s' % resultDic	
+		server.sendmail(user, user, msg)
 		server.close()
 		print "email successfully sent.\n"
 	except:
@@ -36,14 +37,17 @@ def send_email(user, pwd):
 # Main function
 def main(argv=None):
 
-	####################
 	# Read Configuration
-	####################
 	config_file = None
 	email = None
 	pw = None
+        
+        # thread count
+	producer_count = range(1,2)
+	consumer_count = range(1,2)
+
 	try:
-		opts,args = getopt.getopt(sys.argv[1:], 'hc:e:p:', ['help', 'config=', 'email=', 'pass='])
+		opts,args = getopt.getopt(sys.argv[1:], 'hc:', ['help', 'config='])
 	except getopt.GetoptError as err:
 		print (err)
 		sys.exit(1)
@@ -54,51 +58,39 @@ def main(argv=None):
 			sys.exit()
 		elif o in ('-c', '--config'):
 			config_file = a
-		elif o in ('-e', '--email'):
-			email = a
-		elif o in ('-p', '--pass'):
-			pw = a
 		else:
 			assert False, 'unhandled option'
 
-	if config_file == None:
-		print "[Error] Please specify the configuration file"
-		usage()
-		sys.exit(1)
+	if config_file != None:
 
-	if email and pw == None:
-		print "[Error] Please specify the password"
-		sys.exit(1)
-	try:
-		with open(config_file) as config_file:
-			config = json.load(config_file)
-	except :
-		print "[Error] Cannot find the [ " + config_file + " ] file"
-		usage()
-		sys.exit(1)
+		try:
+			with open(config_file) as config_file:
+				config = json.load(config_file)
+		except :
+			print "[Error] Cannot find the [ " + config_file + " ] file"
+			usage()
+			sys.exit(1)
 
-	producer_count = config["producer"]["count"]
-	consumer_count = config["consumer"]["count"]
+		email = config["notification"]["id"]
+		pw = config["notification"]["pw"]
 
-	# write system information
+	# Write system information
 	date = strftime("%Y%m%d-%H%M", localtime())
 	output_path = "./results/%s" % date
 	if not os.path.isdir(output_path):
 		os.makedirs(output_path)
 
-	sysInfoFile = open("%s/%s" % (output_path, systemInfoFName), 'w')
-	sysInfoFile.write("--------------------------------------------------\n")
+	expInfoFile = open("%s/%s" % (output_path, expInfoFName), 'w')
+	expInfoFile.write("Machine Information\n")
+	expInfoFile.write("--------------------------------------------------\n")
 	cmd = "lscpu" 
 	p = Popen( shlex.split(cmd), stdout=PIPE )
 	for line in p.stdout.readlines():
-		sysInfoFile.write(line)
+		expInfoFile.write(line)
 	p.wait()
 
-	sysInfoFile.close()
-
 	# write experiment information
-	expInfoFile = open("%s/%s" % (output_path, expInfoFName), 'w')
-	expInfoFile.write("Expreriment Information\n")
+	expInfoFile.write("\nExpreriment Information\n")
 	expInfoFile.write("--------------------------------------------------\n")
 	expInfoFile.write("1. Expreriment List\n")
 	for i in range( 0, len(TEST_LIST) ):
@@ -112,19 +104,13 @@ def main(argv=None):
 	expInfoFile.write("\n\n3. Iterate Count: %d" % iterCnt) 
 	expInfoFile.write("\n\n4. Experiment detail info") 
 
-
-	####################
 	# Running benchmark
-	####################
-	try_cnt=1
 	for i in range( 0, len(TEST_LIST) ):
 		for ii in range(0, len(producer_count) ):
 			for t in range( 0, iterCnt ):
 				for o in range (0, len(opt)):
-					##########################
-					# Setup output directories
-					##########################
 
+					# Setup output directories
 					exp_output_path = "%s/%s" % (output_path, TEST_LIST[i])
 
 					print "exp_output_path: %s\n" % exp_output_path
@@ -134,10 +120,11 @@ def main(argv=None):
 					outputFile = open("%s/%s-%s-%s-%s-%s" % (exp_output_path, TEST_LIST[i], producer_count[ii], consumer_count[ii], t, opt[o]), 'w')
 					print "outputFile: %s" % outputFile
 
-					print "\nDropping page cache"
-					cmd = "echo 3 | sudo tee /proc/sys/vm/drop_caches"
-					p = Popen( shlex.split(cmd) )
-					p.wait()
+                                        # Dropping cache
+					#print "\nDropping page cache"
+					#cmd = "echo 3 | sudo tee /proc/sys/vm/drop_caches"
+					#p = Popen( shlex.split(cmd) )
+					#p.wait()
 
 					start_time = time.time()
 
@@ -154,17 +141,16 @@ def main(argv=None):
 					end_time = time.time()
 					print "Time elapsed: ", end_time-start_time
 					print "Outpute path: ", output_path
-					expInfoFile.write("\n[%d] %s" % (try_cnt, TEST_LIST[i])) 
+					expInfoFile.write("\n[%d] %s" % (t+1, TEST_LIST[i])) 
 					expInfoFile.write("\n  * Output path\n %s" % exp_output_path) 
 					expInfoFile.write("\n  * Time elapsed\n %rs" % (end_time-start_time))
-					try_cnt += 1
 					print "Done"			
-					print "Waiting... to warm the states"
-					time.sleep(40)
+					#print "Waiting... to warm the states"
+					#time.sleep(40)
 					
 	expInfoFile.close()
 	# send email notification
-	send_email(email, pw)
+	send_email(email, pw, os.path.abspath(output_path))
 
 if __name__ == '__main__' :
 	sys.exit(main())
